fd794a330c101792777b818eb022b2c4507ba943
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2024-05-14 14:27:29 -0400

    better impl

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -1,10 +1,13 @@
 import { Link } from '@inertiajs/react';
 
 export default function NavLink({ href, active, children, disabled }) {
-  
+    if (disabled) {
+        return null;
+    }
+
     return (
         <Link
-            href={disabled ? '#' : href}
+            href={href}
             className={
                 active
                     ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
